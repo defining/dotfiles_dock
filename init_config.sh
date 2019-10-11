2fde685d171d1c33a11675579050cfd352476c45
Soufiane Amzur <avantoo@hotmail.com>
2019-10-12 00:19:32 +0200

    Modification for script

--- init_config.sh
+++ init_config.sh
@@ -1,3 +1,3 @@
-cp -f ./.zshrc ~/.zshrc
-cp -f ./.vimrc ~/.vimrc
-cp -f ./dpoggi.zsh-theme ~/.oh-my-zsh/themes/dpoggi.zsh-theme
+cp -f .zshrc ~/.zshrc
+cp -f .vimrc ~/.vimrc
+cp -f dpoggi.zsh-theme ~/.oh-my-zsh/themes/dpoggi.zsh-theme
